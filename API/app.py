f02d467bef010c6c055d3ccd146d0615f101f0f8
ASK-03 <abhisheksinghkushwaha22@gmail.com>
2024-02-12 19:56:18 +0530

    integrated frontend and backend for mp3 file

--- API/app.py
+++ API/app.py
@@ -16,7 +16,7 @@ def save_audio():
     try:
         audio_data = process_audio(request=request)
 
-        filename = os.path.join(audio_folder, "audio_file.wav")
+        filename = os.path.join(audio_folder, "audio_file.mp3")
         with open(filename, 'wb') as file:
             file.write(audio_data)
         
--- API/middlewares/process_audio.py
+++ API/middlewares/process_audio.py
@@ -1,8 +1,16 @@
-# middlewares/audio_processing.py
+# middlewares/process_audio.py
+from pydub import AudioSegment
+import io
+
 def process_audio(request):
     try:
         # Get the raw binary data from the request
-        audio_data = request.get_data()
+        audio_data = request.files["audio"]
+
+        audio_data = audio_data.read()
+
+        print(audio_data)
+
         return audio_data
     except Exception as e:
         return f"Error processing audio: {str(e)}", 500
