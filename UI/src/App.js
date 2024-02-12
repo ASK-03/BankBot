f71a886e61c76a1a95db03f360a4e210ad704fa3
ASK-03 <abhisheksinghkushwaha22@gmail.com>
2024-02-12 20:14:05 +0530

    bug fix
    Signed-off-by: ASK-03 <abhisheksinghkushwaha22@gmail.com>

---
+++ UI/src/App.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "./App.css";
+import MicRecorder from "mic-recorder-to-mp3";
+import axios from "axios";
+
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRecording: false,
+      blobURL: "",
+      isBlocked: false,
+    };
+  }
+
+  start = () => {
+    if (this.state.isBlocked) {
+      console.log("Permission Denied");
+    } else {
+      Mp3Recorder.start()
+        .then(() => {
+          this.setState({ isRecording: true });
+        })
+        .catch((e) => console.error(e));
+    }
+  };
+
+  stop = () => {
+    Mp3Recorder.stop()
+      .getMp3()
+      .then(([buffer, blob]) => {
+        const blobURL = URL.createObjectURL(blob);
+        this.setState({ blobURL, isRecording: false });
+
+        const formData = new FormData();
+        formData.append("audio", blob, "filename");
+
+        axios
+          .post("http://127.0.0.1:5000/audio/save", formData)
+          .then((response) => {
+            console.log("Audio uploaded successfully:", response.data);
+          })
+          .catch((error) => {
+            console.error("Error uploading audio:", error);
+          });
+      })
+      .catch((e) => console.log(e));
+  };
+
+  componentDidMount() {
+    navigator.getUserMedia(
+      { audio: true },
+      () => {
+        console.log("Permission Granted");
+        this.setState({ isBlocked: false });
+      },
+      () => {
+        console.log("Permission Denied");
+        this.setState({ isBlocked: true });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.start} disabled={this.state.isRecording}>
+            Record
+          </button>
+          <button onClick={this.stop} disabled={!this.state.isRecording}>
+            Stop
+          </button>
+          <audio src={this.state.blobURL} controls="controls" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
